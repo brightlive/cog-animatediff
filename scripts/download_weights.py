1248d8598c126a7f22b1111304102ff6c9d9a697
Brian <71035007+bright-brian@users.noreply.github.com>
2023-08-22 19:54:31 -0700

    Fix typo in script

--- scripts/download_weights.py
+++ scripts/download_weights.py
@@ -12,7 +12,7 @@ os.system("cd models_cache")
 
 os.system("git clone --branch fp16 https://huggingface.co/runwayml/stable-diffusion-v1-5")
 os.system("cd stable-diffusion-v1-5")
-os.system("apt apt-get install git-lfs")
+os.system("apt-get install git-lfs")
 os.system("git lfs install")
 os.system("git lfs pull")
 print("Now you should replace the vocab file in models_cache/stable-duffusion-v1-5/tokenizer")
