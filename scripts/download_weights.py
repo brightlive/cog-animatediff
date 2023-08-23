e4d3a90d27d552823dd6cdc48f40c04de3297198
Brian <71035007+bright-brian@users.noreply.github.com>
2023-08-22 17:35:48 -0700

    Try download_weights script

---
+++ scripts/download_weights.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import sys
+
+# append project directory to path so predict.py can be imported
+sys.path.append('.')
+
+os.system("mkdir models_cache")
+os.system("cd models_cache")
+
+os.system("git clone --branch fp16 https://huggingface.co/runwayml/stable-diffusion-v1-5")
+os.system("cd stable-diffusion-v1-5")
+os.system("apt apt-get install git-lfs")
+os.system("git lfs install")
+os.system("git lfs pull")
+print("Now you should replace the vocab file in models_cache/stable-duffusion-v1-5/tokenizer")
